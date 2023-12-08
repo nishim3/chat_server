1593ee36d4d2581ec272eeed08f1b0fb6434b373
Nishim Goyal <nishimgoyal@Nishim.local>
2023-12-08 11:58:59 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use tokio::net::TcpListener;
+use tokio::io::{AsyncWriteExt, BufReader, AsyncBufReadExt};
+use tokio::sync::broadcast;
+#[tokio::main]
+async fn main()
+{
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (tx, _rx)= broadcast::channel(10);
+    loop
+    {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx=tx.clone();
+        let mut rx=tx.subscribe();
+        tokio::spawn(async move
+        {
+            let (reader, mut writer)=socket.split();
+            let mut reader= BufReader::new(reader);
+            let mut line= String::new();
+            loop
+            {
+                tokio::select! 
+                {
+                    result=reader.read_line(&mut line) =>
+                    {
+                        if result.unwrap() == 0
+                        {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result=rx.recv() =>
+                    {
+                        let  (msg, msg_addr)=result.unwrap();
+                        if msg_addr != addr
+                        {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
+
